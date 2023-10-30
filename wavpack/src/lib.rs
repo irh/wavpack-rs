6e8cc914f5873c474fd4ac66cd0abd8269b5117b
Ian Hobson <ian.r.hobson@gmail.com>
2023-10-30 17:44:41 +0100

    Update to Wavpack 5.6.0

--- wavpack/src/lib.rs
+++ wavpack/src/lib.rs
@@ -66,22 +66,22 @@ fn option_to_ptr<T>(x: &mut Option<T>) -> *mut T {
 /// Return the WavPack library version in a packed integer.
 ///
 /// Bits 0-7 give the micro version, bits 8-15 give the minor version, and
-/// bits 16-23 give the major version. As of this writing the version is 5.0.0.
+/// bits 16-23 give the major version. As of this writing the version is 5.6.0.
 /// ```
 /// use wavpack::get_library_version;
 /// let version = get_library_version();
-/// assert_eq!(version, 0x050400);
+/// assert_eq!(version, 0x050600);
 /// ```
 pub fn get_library_version() -> u32 {
     unsafe { WavpackGetLibraryVersion() }
 }
 /// Return the WavPack library version as a string.
 ///
-/// As of this writing this is "5.0.0".
+/// As of this writing this is "5.6.0".
 /// ```
 /// use wavpack::get_library_version_string;
 /// let version = get_library_version_string();
-/// assert_eq!(version, "5.4.0");
+/// assert_eq!(version, "5.6.0");
 /// ```
 pub fn get_library_version_string() -> &'static str {
     let c_str = unsafe { CStr::from_ptr(WavpackGetLibraryVersionString()) };
