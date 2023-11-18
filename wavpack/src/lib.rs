620681ce6f0ee37883f499cadf78c92caad48aa6
Ian Hobson <ian.r.hobson@gmail.com>
2023-11-18 08:52:06 +0100

    Export the WavpackHeader struct from wavpack_sys

--- wavpack/src/lib.rs
+++ wavpack/src/lib.rs
@@ -8,4 +8,5 @@ mod writer;
 pub use error::{Error, Result};
 pub use reader::{TagData, WavpackRead, WavpackReader, WavpackReaderBuilder};
 pub use version::{get_library_version, get_library_version_string};
+pub use wavpack_sys::WavpackHeader;
 pub use writer::{WavpackWrite, WavpackWriter, WavpackWriterBuilder};
